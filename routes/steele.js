93dc270e6250613854ac54215cb0a8d5f9f6d574
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2018-01-26 17:42:57 +0000

    Data store added

--- routes/steele.js
+++ routes/steele.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var redis = require("redis"),
+    client = redis.createClient();
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('steele', { title: 'Joshua Steele' });
 });
+
+/* POST data to the Redis backend */
+router.post('/', function(req, res, next) {
+  setData(req);
+});
+
+/*
+ Function to store the data
+*/
+function setData(req) {
+  var milliseconds = (new Date).getTime();
+
+  var key = req.body.key;
+  var _data = req.body.data;
+  
+  client.hset('steele::'+key, milliseconds, _data, redis.print);
+
+}
 
 module.exports = router;
