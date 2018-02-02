1cb27c953b0e4201906dc6b39e30f1e177d624d2
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2018-02-02 15:48:18 +0000

    Updating the fake endpoint

--- routes/steele.js
+++ routes/steele.js
@@ -14,6 +14,29 @@ router.post('/', function(req, res, next) {
   setData(req);
 });
 
+// fake endpoint to test the data export
+router.get('/data/:type', function(req,res, next) {
+res.set('Content-Type', 'application/xml');
+var xml = '<dso>';
+xml += '<head><publication type="'+ req.params.type +'"/></head>';
+if (req.params.type == 'collection') {
+  xml += '<body>' + fakeCollection() + '</body>';
+} else {
+  xml += '<body>' + fakeAggregation() + '</body>';
+}
+xml += '</dso>';
+res.send(xml);
+});
+
+function fakeCollection() {
+  return '<part id="1"><note freq="440" dur="0.3" vol="0.5" /><note freq="441" dur="0.3" vol="0.6" /></part>' +
+         '<part id="2"><note freq="440" dur="0.3" vol="0.5" /><note freq="230" dur="0.2" vol="0.5" /></part>';
+}
+
+function fakeAggregation() {
+  return '<part id="1"><note freq="440.5555" dur="0.3" vol="0.55" /><note freq="330.08" dur="0.25" vol="0.6" /></part>';
+}
+
 /*
  Function to store the data
 */
