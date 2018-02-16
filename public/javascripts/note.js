0c2d7c7c2cb1786f80722a702dfcb53124c98ac5
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2018-02-16 15:02:10 +0000

    beginning to refactor note

--- public/javascripts/note.js
+++ public/javascripts/note.js
@@ -1,29 +1,16 @@
   var Note = function() {
 
-      //this.frequency = frequency;
-      //this.note_length = function(note_length) { return 1/note_length};
-      //this.vol = _gain_note(loud);
-
-    function setLength(duration) {
-      return 1/duration;
-    }
-
-    function setGain(gain) {
-      return (gain == '0') ? 0.5 : (gain + 0.1);
-    }
-
-    function start(audioCtx, frequency, note_length, loud, _old, _duration) {
+    function start(audioCtx, frequency, note_length, volume, id) {
 
       var oscillator = audioCtx.createOscillator();
       var gainNode = audioCtx.createGain();
       gainNode.connect(audioCtx.destination);
-      gainNode.gain.value = setGain(loud);
+      gainNode.gain.value = volume;
       oscillator.type = "sine";
       oscillator.frequency.value = frequency;
-      oscillator.start(_old);
-      console.log('{ duration:' + (_old + (0.001+ _duration)) + ', volume: ' + loud + ', pitch:' + frequency + ', id: ' + _old +'}');
-      _model.push('{ duration:' + (_old + (0.001+ _duration)) + ', volume: ' + loud + ', pitch:' + frequency + ', id: ' + _old +'}');
-      oscillator.stop(_old + (0.001 + _duration));
+      oscillator.start(id);
+      
+      oscillator.stop(id + (0.001 + note_length));
       oscillator.connect(audioCtx.destination);
     };
 
