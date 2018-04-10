3191b25f5522f73f7669a921ecdddfb04f9c2996
Iain Emsley <iain.emsley@kellogg.ox.ac.uk>
2018-04-10 15:49:22 +0100

    Updating the annotation prov store

--- routes/provenance.js
+++ routes/provenance.js
@@ -3,6 +3,12 @@ var N3 = require('n3');
 var router = express.Router();
 
 
+router.post('/comment', function(req, res, next) {
+  let uri = JSON.parse(req.body.data);
+  console.log(uri);
+  res.status == 200;
+});
+
 /* GET prov graph for this id .
 @todo: change this to a hah function (rfc6920)
  */
@@ -47,11 +53,32 @@ function storeProvGraph(graph, hashkey) {
   client.hset(hashkey, 'prov', graph, redis.print);
 }
 
+function createAnnotationGraph() {
+/*
+@prefix dc: <http://purl.org/dc/terms/> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@prefix ns0: <http://www.w3.org/ns/oa#> .
+@prefix dc11: <http://purl.org/dc/elements/1.1/> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+
+<http://example.org/anno>
+  a <http://www.w3.org/ns/oa#Annotation> ;
+  dc:created "2018-04-10T14:34:20.032Z"^^xsd:dateTime ;
+  ns0:hasBody [
+    a <http://njh.me/volume> ;
+    dc11:format "text/plain"^^xsd:string ;
+    rdf:value "0.5318603611900158"^^xsd:string
+  ] ;
+  ns0:hasTarget <http://127.0.0.1/sonify/> .
+*/
+}
+
+
 function createSonificationProvGraph(uri) {
    writer.addTriple( 'http://example.org/sonification', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.w3.org/ns/prov#Entity');
    writer.addTriple( 'http://example.org/sonification', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#about', '"http://127.0.0.1:3000/sonify/'+uri);
    writer.addTriple( 'http://example.org/sonification', 'http://www.w3.org/ns/prov#wasGeneratedBy', 'http://example.org/note');
-   writer.addTriple( 'http://example.org/sonification', 'http://www.w3.org/ns/prov#wasDerivedFrom', 'http://example.org/data');
+   //writer.addTriple( 'http://example.org/sonification', 'http://www.w3.org/ns/prov#wasDerivedFrom', 'http://example.org/data');
    //add note
    writer.addTriple( 'http://example.org/note', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://www.w3.org/ns/prov#Activity');
    writer.addTriple( 'http://example.org/note', 'http://www.w3.org/ns/prov#used', 'http://example.org/data');
